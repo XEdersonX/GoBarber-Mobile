d6cb8448b1ce59805d7918cd6f65543b04ebeefb
Ederson Luís <edergp2009@gmail.com>
2020-06-08 09:21:08 -0300

    Configurando a estrutura.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native'; // nossa view que é a div em branco.
+
+// construir o componente chamado App, que vai ser fuction componente
+const App: React.FC = () => <View />;
+
+export default App;
