e87d6ab6bcb132b663409a0f828503b18ff68bac
Ederson Luís <edergp2009@gmail.com>
2020-06-08 10:12:17 -0300

    Padrões de estilo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native'; // nossa view que é a div em branco.
+import { View } from 'react-native'; // nossa view que é a div em branco.
 
 // construir o componente chamado App, que vai ser fuction componente
 const App: React.FC = () => <View />;
