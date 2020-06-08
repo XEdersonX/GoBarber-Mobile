d48e57d14c31ff18edb0401d67c4797a30938c33
Ederson Luís <edergp2009@gmail.com>
2020-06-08 17:07:29 -0300

    Configurando navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, StatusBar } from 'react-native'; // nossa view que é a div em branco.
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes'; // já puxa o arquivo index de form automatica
 
 // construir o componente chamado App, que vai ser fuction componente
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
